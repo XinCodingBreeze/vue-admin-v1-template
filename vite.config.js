8dff39e13c59792cd9010b13372d9832ea0b2581
XinBreeze <19931845446@163.com>
2026-04-30 15:36:01 +0800

    build: 优化 Vite 构建配置，统一引号风格并移除调试日志

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,10 @@ import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 // 自动导入模块
 import AutoImport from "unplugin-auto-import/vite";
-import viteCompression from 'vite-plugin-compression';
+import viteCompression from "vite-plugin-compression";
 // https://vitejs.dev/config/
-export default ({ command, mode }) => {
-  console.log(command, mode, "command, mode");
-  let PropxUrl = loadEnv(mode, process.cwd()).VITE_APP_API_URL;
+export default ({ mode }) => {
+  const proxyUrl = loadEnv(mode, process.cwd()).VITE_APP_API_URL;
 
   return defineConfig({
     plugins: [
@@ -17,13 +16,13 @@ export default ({ command, mode }) => {
       }),
       viteCompression({
         threshold: 1024 * 20,
-        algorithm: 'gzip',
-        ext: '.gz',
+        algorithm: "gzip",
+        ext: ".gz",
         compressionOptions: {
           level: 9, // 压缩级别，范围为 1-9，9 为最高压缩率
         },
         // filter: /\.(js|css|html|json)$/i, // 仅压缩 JavaScript、CSS、HTML 和 JSON 文件
-      })
+      }),
     ],
     build: {
       rollupOptions: {
@@ -34,15 +33,15 @@ export default ({ command, mode }) => {
           chunkFileNames: "js/[name]-[hash].js",
           // 根据不同资源类型自定义输出路径和文件名
           assetFileNames(assetInfo) {
-            const info = assetInfo.name.split('.');
+            const info = assetInfo.name.split(".");
             const ext = info[info.length - 1];
 
-            if (ext === 'css') {
+            if (ext === "css") {
               return "css/[name]-[hash].css";
             }
 
             // 定义图片文件扩展名列表
-            const imgExts = ['png', 'jpg', 'jpeg', 'gif', 'ico', 'svg', 'webp'];
+            const imgExts = ["png", "jpg", "jpeg", "gif", "ico", "svg", "webp"];
             // 图片文件放入imgs目录
             if (imgExts.includes(ext)) {
               return `imgs/[name]-[hash].${ext}`;
@@ -65,7 +64,7 @@ export default ({ command, mode }) => {
       // host: '0.0.0.0', // 设置主机地址
       proxy: {
         "/api": {
-          target: PropxUrl,
+          target: proxyUrl,
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, ""),
         },
